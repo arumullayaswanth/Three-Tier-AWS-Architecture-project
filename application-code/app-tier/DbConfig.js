dea72832801d5b072c987a55d3cdcf7d9d83efa1
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-04-15 13:24:41 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tireproject.c0n8k0a0swtz.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'yaswanth123',
+    DB_DATABASE : 'webappdb'
+});
